722bd8bf595049e1a6dbecbddab94aea43c26432
cassetteelectrodomestico <castelaoalferez.erick@gmail.com>
2020-12-09 19:14:27 -0600

    me dijo maritza

--- index.py
+++ index.py
@@ -1,11 +1,24 @@
+global redes
+
+from static.py.mysql_final import run_query
+from static.py.predict import Predictor
+
+redes = []
+print("Ya empece")
+print("Iniciando " + str(1))
+redes.append(Predictor(1))
+print("Ya acabé uwu")
+
+
+
+
 from flask import Flask, render_template
 from flask import request
 import datetime
 import numpy as np
-from static.py.mysql_final import run_query
-from static.py.predict import Predictor
 
-global redes
+
+
 
 app = Flask(__name__)
 
@@ -14,11 +27,8 @@ app = Flask(__name__)
 
 
 def home():
-	redes = []
-	for i in range(5):
-		redes.append(Predictor(i + 1))
-	print("Ya acabé uwu")
-    #cantidad = 0
+	cantidad = 0
+	print("pagina cargada cx")
 	return render_template('home.html', cantidad=cantidad)
 
 @app.route('/nuevoSorteo', methods = ['POST'])
@@ -33,11 +43,12 @@ def nuevoSorteo():
 	dia = int(a_parts[2])
 	precio = int(data['precio'])
 	index_red = int(data['sorteo']) - 1
-    #(a - b).split(" ")[0]
-    #dediez = Predictor(data['sorteo'])
-    #modifigar las fecheas para que lo reciba la funcion
-    #cantidad = dediez.predict(test_data)#aqui las fechas
-	cantidad = redes[index_red].predict(np.array([dias, mes, dia, precio]))
+	#(a - b).split(" ")[0]
+	#dediez = Predictor(data['sorteo'])
+	#modifigar las fecheas para que lo reciba la funcion
+	#cantidad = dediez.predict(test_data)#aqui las fechas
+	cantidad = (redes[index_red].predict(np.array([[dias, mes, dia, precio]])))
+	cantidad = cantidad[0][0]
 	return render_template('home.html',cantidad = cantidad)
 
 if __name__ == '__main__':
